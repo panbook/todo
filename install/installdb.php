8dad260c422817fdd34b152bb3322810c2f065e3
Jeremi Brzegowy <jeremibrzegowy2@gmail.com>
2025-01-30 21:42:36 +0100

    zmiana nazwy tabeli i kolumn

--- install/installdb.php
+++ install/installdb.php
@@ -14,13 +14,12 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 
-$sql = "CREATE TABLE MyGuests (
+$sql = "CREATE TABLE Kartki (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+tresc varchar(30)
 )";
+
+
 
 if ($conn->query($sql) === TRUE) {
   echo "Table MyGuests created successfully";
