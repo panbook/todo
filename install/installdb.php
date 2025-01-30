d68d7028c92a255bb22bf5807ddc6e7d23502360
Jeremi Brzegowy <jeremibrzegowy2@gmail.com>
2025-01-30 21:34:25 +0100

    adding table while executing php

--- install/installdb.php
+++ install/installdb.php
@@ -1,21 +1,32 @@
 <?php
 
-
 $servername = "sql.freedb.tech";
 $username = "freedb_todoadmin";
 $password = "GhxSZq8$5BavF5#";
+$dbname = "freedb_todosons";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
 
-// Check connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
 
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
 
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
 
-
-
+$conn->close();
 ?>
