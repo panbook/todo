2d6a43e546262a373c313b32ca888e98c81e1c4b
Jeremi Brzegowy <jeremibrzegowy2@gmail.com>
2025-01-31 17:20:56 +0100

    mysqli_connect try php

--- install/installdb.php
+++ install/installdb.php
@@ -6,7 +6,7 @@ $password = "TymJerAre123";
 $dbname = "todosons";
 
 
-$conn = mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 
 if ($conn->connect_error) {
