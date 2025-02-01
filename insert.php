980bed1a25e4b7932337ffdd449e371163a46b2d
Jeremi Brzegowy <jeremibrzegowy2@gmail.com>
2025-02-01 16:19:06 +0100

    connecting and inserting into database in php

--- insert.php
+++ insert.php
@@ -1,5 +1,21 @@
 <?php
 
 $textValue = $_POST['textValue'];
+$servername = "127.0.0.1";
+$username = "root";
+$password = "TymJerAre123";
+$dbname = "todosons";
+
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+$sql = "INSERT INTO Kartki(`tresc`) VALUES ('$textValue')";
 
 ?>
